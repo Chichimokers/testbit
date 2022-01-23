9cfec88897be25c3d3f4397de57891234653021c
Ernes <ernestico833@gmail.com>
2022-01-24 00:31:04 +0100

    oficial

--- config.py
+++ config.py
@@ -1,12 +1,21 @@
 import json
 
+from requests import api
+
+global arroba
 arroba=""
 
+global username
 username=""
 
-apibot=""
+global apibot
 
+apibot =""
+
+global password
 password=""
+
+global nube
 
 nube=""
 
@@ -31,5 +40,11 @@ def Loadjson():
     nube=jsons["nube"]
 
     apibot=jsons["API"]
+
+    print(arroba)
+    print(username)
+    print(password)
+    print(nube)
+    print(apibot)
 
     pass
