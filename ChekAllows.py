ae31196fd3461423ef62f08e0fa2d6a592cd672a
Ernes <ernestico833@gmail.com>
2022-01-24 00:53:26 +0100

    oficial

--- ChekAllows.py
+++ ChekAllows.py
@@ -4,8 +4,11 @@ from config import Config
 paths = os.path.dirname(os.path.abspath(__file__))
 
 def EstasPermitiado(update):
+    
     configuracion = Config()
+
     if("@"+update.message.chat.username == configuracion.arroba):
+
         return True
 
     else:
