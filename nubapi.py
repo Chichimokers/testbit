13b4ad2fad09a20c853e6e6eb43461ea909ed605
Ernes <ernestico833@gmail.com>
2022-01-26 15:29:54 +0100

    oficial

--- nubapi.py
+++ nubapi.py
@@ -764,27 +764,8 @@ class NubApi():
               porcent = int(monitor.bytes_read/size*100)
 
               cambio = str("Uploading "+str(CheckSize(monitor.bytes_read))+" de "+str(CheckSize(size))+" "+str(porcent)+"%") 
+              
               print(s)
-
-              if(mensajeuno.text.split(" ")[-1] != str(str(porcent)+"%")):
- 
-                   lista = [1,30,60,80,90,100]
-
-                   for e in lista:
-
-                     if(e == int(porcent)):
-
-                         print("Se cambio")
-                         try:
-                          lastporcent = str(porcent)
-
-                          mensajeuno.text = cambio
-                         
-                          mensajeuno.edit_text(cambio)
-                         except:
-                             print("No se puedo cambiar el mensaje")
-
-          
 
               pass
           values = {'sesskey': sesskey,'repo_id':'4','author':self.Autor,'savepath':'/','title':name,'itemid':query["itemid"],'ctx_id':query["ctx_id"],"repo_upload_file": (name,open(pathfile,'rb'))}
